0640ee3773698c8bc7ae2918f406bd1e15de9e4e
ImmaDevGal <paolatilve@gmail.com>
2019-08-04 18:40:39 -0500

    Adding new clases to the model designed

---
+++ UBL21InvoiceGenerator/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UBL21InvoiceGenerator
+{
+    class Document
+    {
+        String documentType { get; set; }
+        List<Tax> taxes;
+        List<WithholdingTax> withholdingTaxes;
+        List<Line> lines;
+        Total total;
+    }
+}
--- UBL21InvoiceGenerator/Line.cs
+++ UBL21InvoiceGenerator/Line.cs
@@ -8,5 +8,13 @@ namespace UBL21InvoiceGenerator
 {
     class Line
     {
+          Int32 id { get; set; }
+        String description { get; set; }
+        Decimal quantity { get; set; }
+        Decimal price { get; set; }
+        Decimal amount { get; set; }
+        Tax tax;
+        WithholdingTax withholdingTax;      
+
     }
 }
